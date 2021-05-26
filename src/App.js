96591a1a7c0f1b7fd284349369cf397a8af86619
Vsumargo <vincent.sumargo@gmail.com>
2021-05-26 11:01:39 +0800

    add basename to browserRouter for gh-page

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
   return (
     <>
       <CssBaseline />
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <Grid container>
           <Grid item xs={2} md={1}>
             <Navbar handleReverse={handleReverse} />
